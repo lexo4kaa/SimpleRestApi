e79dbb667b10f5faac42b3ddbbdb4336be7c8ff2
lexo4kaa <alesha.krupenko@gmail.com>
2024-02-18 19:15:11 +0300

    Added request params to the HelloWorld method

--- src/main/java/com/demoproject/restapi/controllers/DemoController.java
+++ src/main/java/com/demoproject/restapi/controllers/DemoController.java
@@ -1,14 +1,16 @@
 package com.demoproject.restapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
     @GetMapping("/hello")
-    public String helloWorld() {
-        return "Hello World!";
+    public String helloWorld(@RequestParam(value = "name", defaultValue = "user") String name,
+                             @RequestParam(value = "city", required = false) String city) {
+        return "Hello " + name + (city == null ? "" : " from " + city) + "!";
     }
 
 }

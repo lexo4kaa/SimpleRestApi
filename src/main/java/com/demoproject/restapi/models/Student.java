426acbca14a31c041b4b51bfe541b6a8e3caf7bb
lexo4kaa <alesha.krupenko@gmail.com>
2024-02-18 20:51:51 +0300

    Added ability to work with MySQL

--- src/main/java/com/demoproject/restapi/models/Student.java
+++ src/main/java/com/demoproject/restapi/models/Student.java
@@ -1,17 +1,26 @@
 package com.demoproject.restapi.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NonNull;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
-    @NonNull
+    @Column(unique = true)
     private String email;
 }
